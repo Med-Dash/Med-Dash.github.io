43615c8050dc442a4eeeda5add11157f108e5011
Nicole Brye <nabrye@yahoo.com>
2023-01-29 14:16:27 -0800

    Preliminary data cleaning pipeline steps

---
+++ src/features.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import pandas as pd
+
+
+def addWeekend(df):
+    
+    '''
+    Adds columns to cleaned Oura data discerning whether or not the day is a weekend
+    
+    :param df: Cleaned Oura DataFrame
+    :returns: Oura DataFrame with 'day_name' and 'is_weekend' columns added
+    '''
+    
+    OUT = OUT.assign(date = pd.to_datetime(OUT['date']))
+    OUT = OUT.assign(day_name = OUT['date'].dt.day_name())
+    OUT = OUT.assign(is_weekend = OUT['day_name'].isin(['Saturday', 'Sunday']))
+    
+    return OUT
+
+
