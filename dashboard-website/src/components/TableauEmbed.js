db224c11ab4b781f95f86f273468ba30819928af
Kamen-R <72962562+Kamen-R@users.noreply.github.com>
2023-02-10 13:59:12 -0800

    Update TableauEmbed.js to include new url to dashboard

--- dashboard-website/src/components/TableauEmbed.js
+++ dashboard-website/src/components/TableauEmbed.js
@@ -5,7 +5,7 @@ const { tableau } = window;
 
 function TableauEmbed () {
     const ref = useRef(null);
-    const url = "https://public.tableau.com/views/Book2_16742399644750/Sheet2?:language=en-US&:display_count=n&:origin=viz_share_link";
+    const url = "https://public.tableau.com/views/LongitudinalDashboard/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
 
     function initViz () {
         new tableau.Viz(ref.current, url);
