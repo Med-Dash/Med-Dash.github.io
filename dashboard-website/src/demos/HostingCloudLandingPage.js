be6c3b110c53565d5c50550a02d0b5e267d58cf9
xxwwyytt1 <xih046@ucsd.edu>
2023-03-07 19:45:30 -0800

    change feature button and text

--- dashboard-website/src/demos/HostingCloudLandingPage.js
+++ dashboard-website/src/demos/HostingCloudLandingPage.js
@@ -3,6 +3,7 @@ import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
 import OurTeam from "components/features/OurTeam";
 import MainFeature from "components/features/TwoColWithButton.js";
+import GoodDietFeature from "components/features/GoodDietFeature.js";
 import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
 import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
 import FAQ from "components/faqs/TwoColumnPrimaryBackground.js";
@@ -22,13 +23,13 @@ export default () => {
       <TableauEmbed />
       {/* <Pricing /> */}
       <MainFeature 
-        subheading="Reliable"
+        subheading=""
         heading="The Importance of Sleep"
         imageSrc={sleepIllustrationImageSrc}
         buttonRounded={false}
       />
-      <MainFeature 
-        subheading="Secure"
+      <GoodDietFeature 
+        subheading=""
         heading="What a Good Diet Can Do For You"
         imageSrc={healthyfoodIllustrationImageSrc}
         buttonRounded={false}
